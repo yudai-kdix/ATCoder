24639d75dce9cd5f27b395677a07d8e103449353
yupon-kdix <yudai.homma222@gmail.com>
2022-06-25 00:03:41 +0900

    ;

---
+++ problem.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * problem
+ */
+public class problem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        String str = sc.next();
+        System.out.print(a + b + c + " ");
+        System.out.print(str);
+    }    
+}
