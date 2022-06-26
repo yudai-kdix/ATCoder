c2cea2a51c85b94a3ec0bd02acea22452f784ba2
yupon-kdix <yudai.homma222@gmail.com>
2022-06-26 22:36:07 +0900

    ABC001

---
+++ ABC001A.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ABC001A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input1 = sc.nextInt();
+        int input2 = sc.nextInt();
+        System.out.println(input1 - input2);
+        sc.close();
+    }
+}
